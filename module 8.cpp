8819eff40e6dfacad1090934ae95e590e435dc32
NismaIrfan850 <nismairfan850@gmail.com>
2025-01-08 23:18:19 +0500

    Add files via upload

---
+++ module 8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    const int ROWS=41;
+    const int COLUMN=6;
+    int alpha; 
+    string data[ROWS][COLUMN]={};
+    int adj[ROWS][ROWS]={};
+    int connectivity[ROWS]={};
+    int rowcount=0;
+    int max=0;
+    cout<<"enter threshold "<<endl;
+    cin>>alpha;
+    for (int i=0;i<ROWS;i++)
+    {
+        if(connectivity[i]>max)
+        {
+            max=connectivity[i];
+        }
+    }
+    cout<<"Broadcast Points "<<endl;
+    for(int i=0;i<ROWS;i++)
+    {
+        if(connectivity[i]==max)
+        {
+            cout<<data[i][0]<<"with"<<connectivity[i]<<" connections"<<endl;
+        }
+    }
+    return 0;
+}
